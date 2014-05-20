ab28ca1de58b966070542099150e4750dcd897a7
honza.skypala <honza.skypala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-20 16:32:26 +0000

    Release 1.2:
    * Loading animation option integrated with WordPress Media Library
    * Added some missing strings to i18n
    * Smaller fixes and optimizations
    
    git-svn-id: https://plugins.svn.wordpress.org/gallery-slice@918044 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/gallery-shortcode-breakdown.php
@@ -0,0 +1,32 @@
+﻿<?php
+/*
+      '\\['                              // Opening bracket
+      . '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
+      . "(gallery)"                     // 2: Shortcode name
+      . '(?![\\w-])'                       // Not followed by word character or hyphen
+      . '('                                // 3: Unroll the loop: Inside the opening shortcode tag
+      .     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+      .     '(?:'
+      .         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
+      .         '[^\\]\\/]*'               // Not a closing bracket or forward slash
+      .     ')*?'
+      . ')'
+      . '(?:'
+      .     '(\\/)'                        // 4: Self closing tag ...
+      .     '\\]'                          // ... and closing bracket
+      . '|'
+      .     '\\]'                          // Closing bracket
+      .     '(?:'
+      .         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+      .             '[^\\[]*+'             // Not an opening bracket
+      .             '(?:'
+      .                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
+      .                 '[^\\[]*+'         // Not an opening bracket
+      .             ')*+'
+      .         ')'
+      .         '\\[\\/\\2\\]'             // Closing shortcode tag
+      .     ')?'
+      . ')'
+      . '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
+*/
+?>
